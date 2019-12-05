ba077bf23ebeda4b59160860906b026b83340136
Chris Lefever <linuxcl4@gmail.com>
2019-12-05 02:48:46 -0500

    Day 5 refactoring

--- day05.py
+++ day05.py
@@ -20,62 +20,44 @@ def run_program(codes, prog_input):
     while prog[ip] != 99:
         code_and_modes = get_opcode_and_modes(prog[ip])
         if code_and_modes[0] == 1:
-            arg1 = prog[prog[ip+1]] if code_and_modes[1] == 0 else prog[ip+1]
-            arg2 = prog[prog[ip+2]] if code_and_modes[2] == 0 else prog[ip+2]
-            prog[prog[ip+3]] = arg1 + arg2
+            params = get_parameters(2, code_and_modes, prog, ip)
+            prog[prog[ip+3]] = params[0] + params[1]
             ip += 4
         elif code_and_modes[0] == 2:
-            arg1 = prog[prog[ip+1]] if code_and_modes[1] == 0 else prog[ip+1]
-            arg2 = prog[prog[ip+2]] if code_and_modes[2] == 0 else prog[ip+2]
-            prog[prog[ip+3]] = arg1 * arg2
+            params = get_parameters(2, code_and_modes, prog, ip)
+            prog[prog[ip+3]] = params[0] * params[1]
             ip += 4
         elif code_and_modes[0] == 3:
             prog[prog[ip+1]] = prog_input
             ip += 2
         elif code_and_modes[0] == 4:
-            arg1 = prog[prog[ip+1]] if code_and_modes[1] == 0 else prog[ip+1]
-            outputs.append(arg1)
+            params = get_parameters(1, code_and_modes, prog, ip)
+            outputs.append(params[0])
             ip += 2
         elif code_and_modes[0] == 5:
-            arg1 = prog[prog[ip+1]] if code_and_modes[1] == 0 else prog[ip+1]
-            if arg1 != 0:
-                ip = prog[prog[ip+2]] if code_and_modes[2] == 0 else prog[ip+2]
-            else:
-                ip += 3
+            params = get_parameters(2, code_and_modes, prog, ip)
+            ip = params[1] if params[0] != 0 else ip + 3
         elif code_and_modes[0] == 6:
-            arg1 = prog[prog[ip+1]] if code_and_modes[1] == 0 else prog[ip+1]
-            if arg1 == 0:
-                ip = prog[prog[ip+2]] if code_and_modes[2] == 0 else prog[ip+2]
-            else:
-                ip += 3
+            params = get_parameters(2, code_and_modes, prog, ip)
+            ip = params[1] if params[0] == 0 else ip + 3
         elif code_and_modes[0] == 7:
-            arg1 = prog[prog[ip+1]] if code_and_modes[1] == 0 else prog[ip+1]
-            arg2 = prog[prog[ip+2]] if code_and_modes[2] == 0 else prog[ip+2]
-            if arg1 < arg2:
-                prog[prog[ip+3]] = 1
-            else:
-                prog[prog[ip+3]] = 0
+            params = get_parameters(2, code_and_modes, prog, ip)
+            prog[prog[ip+3]] = 1 if params[0] < params[1] else 0
             ip += 4
         elif code_and_modes[0] == 8:
-            arg1 = prog[prog[ip+1]] if code_and_modes[1] == 0 else prog[ip+1]
-            arg2 = prog[prog[ip+2]] if code_and_modes[2] == 0 else prog[ip+2]
-            if arg1 == arg2:
-                prog[prog[ip+3]] = 1
-            else:
-                prog[prog[ip+3]] = 0
+            params = get_parameters(2, code_and_modes, prog, ip)
+            prog[prog[ip+3]] = 1 if params[0] == params[1] else 0
             ip += 4
     return outputs
 
+
+def get_parameters(num, code_and_modes, prog, ip):
+    return [prog[prog[ip+i]] if code_and_modes[i] == 0 else prog[ip+i] for i in range(1, num+1)]
+
+
 def get_opcode_and_modes(code):
-    code_str = str(code)
-    if len(code_str) == 5:
-        return (int(code_str[3:]), int(code_str[2]), int(code_str[1]), int(code_str[0]))
-    elif len(code_str) == 4:
-        return (int(code_str[2:]), int(code_str[1]), int(code_str[0]), 0)
-    elif len(code_str) == 3:
-        return (int(code_str[1:]), int(code_str[0]), 0, 0)
-    else:
-        return (int(code_str), 0, 0, 0)
+    code_str = str(code).rjust(5, '0')
+    return (int(code_str[3:]), int(code_str[2]), int(code_str[1]), int(code_str[0]))
 
 
 def main():
